68d670cdd020e7bf48034481a90f981b082c0954
AslamBro <aslamibragimov68@gmail.com>
2025-08-26 23:12:31 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Hero from "./pages/Hero";
 import SingleMovie from "./pages/SingleMovie";
 
+
 function App() {
   return (
     <Routes>
@@ -13,3 +14,14 @@ function App() {
 
 
 export default App;
+
+// const App = () => {
+//   return (
+//     <div>
+//       <SearchPart/>
+//     </div>
+//   )
+// }
+
+// export default App
+
