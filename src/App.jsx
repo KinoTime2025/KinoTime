c5bbab7151f04b1f85c9003d7e16d399e78a7709
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-08-28 12:20:10 +0500

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import { Route, Routes } from "react-router-dom";
-import Hero from "./pages/Hero";
-import SingleMovie from "./pages/SingleMovie";
+// import { Route, Routes } from "react-router-dom";
+// import Hero from "./pages/Hero";
+// import SingleMovie from "./pages/SingleMovie";
 
 
-function App() {
+
+// function App() {
+  //   return (
+    //     <Routes>
+    //       <Route path="/" element={<Hero/>}/>
+    //       <Route path="/singleMovie/:id" element={<SingleMovie/>}/>
+    //     </Routes> 
+    //   );
+    // }
+    
+    
+    // export default App;
+
+
+import SearchPart from "./pages/SearchPart"
+
+const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Hero/>}/>
-      <Route path="/singleMovie/:id" element={<SingleMovie/>}/>
-    </Routes> 
-  );
+    <div>
+      <SearchPart/>
+    </div>
+  )
 }
 
-
-// export default App;
-
-// const App = () => {
-//   return (
-//     <div>
-//       <SearchPart/>
-//     </div>
-//   )
-// }
-
-// export default App
+export default App
 
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -41,12 +41,12 @@ const Hero = () => {
                 style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.6)), url('https://kinotime.world${movie.backdrop}')` }} >
               </div>
 
-              <div className="relative z-10 w-[400px] pl-[50px] pt-25 pb-5">
-                <h2 className="sm:text-[35px] text-[18px] font-medium sm:text-white text-blue-800 sm:font-bold text-center sm:text-start w-auto sm:w-[500px] mb-[40px]">
+              <div className="relative p-0 text-center sm:text-start sm:mx-0 mx-auto  z-10 w-[400px] sm:pl-[50px] sm:pt-25 pb-5">
+                <h2 className="sm:text-[35px] text-[24px] font-bold text-center sm:text-start w-auto sm:w-[500px] mb-[40px]">
                   {movie.title}
                 </h2>
 
-                <div className="flex items-center gap-[10px] mb-[18px]">
+                <div className="sm:flex hidden items-center gap-[10px] mb-[18px]">
                   <div className="flex items-center gap-[5px]">
                     <ImdbIcon />
                     <p className="font-bold text-[18px]">
@@ -57,24 +57,24 @@ const Hero = () => {
                   <LogoTvIcon />
                 </div>
 
-                <p className="font-semibold text-start w-full line-clamp-1 text-[16px]">
+                <p className="font-semibold text-center sm:text-start sm:mx-0 mx-auto w-[80%] sm:w-full line-clamp-1 text-[16px]">
                   {movie.genres.map(g => g.name).join(", ")}
                 </p>
 
-                <div className="mt-[30px] flex items-center gap-[15px]">
+                <div className="relative sm:static mt-[30px] sm:justify-start justify-center flex items-center gap-[15px]">
 
                   <Link to={`/singleMovie/${item.id}`} className="w-[151px] h-[50px] flex items-center justify-center font-medium text-[18px] cursor-pointer text-white bg-[rgb(82,176,56)] rounded-lg text-center ">
                     Ko'rish
                   </Link>
 
-                  <div className="relative">
+                  <div className="sm:relative static">
                     <button onClick={() => setMenu(prev => !prev)} className="text-white w-[50px] h-[49px] rounded-lg flex items-center justify-center bg-[#849dab42]  hover:bg-[#3748086b5] duration-300 cursor-pointer">
                       <ShareIcon />
                     </button>
 
 
                     {menu && (
-                      <div className="absolute left-0 top-0 mt-10 mb-10 w-[214px] rounded-md bg-[rgb(29,31,30)] backdrop-blur-[5px] z-50 overflow-hidden">
+                      <div className="absolute mx-auto right-0 left-0 top-0 mt-13 sm:mt-10 mb-10 w-[214px] rounded-md bg-[rgb(29,31,30)] backdrop-blur-[5px] z-50 overflow-hidden">
                         <a
                           className="flex items-center gap-[15px] text-[16px] p-3 duration-300 hover:bg-[rgb(52,54,53)] rounded-tl-md rounded-tr-md"
                           href="#">
